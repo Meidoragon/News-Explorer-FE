4feed8058d62f6fbeb60c9353f86ac1253e4b37d
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2024-04-09 13:10:58 -0500

    UPDATE: App to include components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,26 @@
 import { useState } from 'react';
 import Header from '../Header/Header';
 import './App.css';
+import Main from '../Main/Main';
+import About from '../About/About';
+import Footer from '../Footer/Footer';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
-    <section className="background-wrapper">
-      <Header
-        isLoggedIn={isLoggedIn}
-        setIsLoggedIn={setIsLoggedIn}
-        userName="PH_User"
-      />
-    </section>
+    <>
+      <section className="background-wrapper">
+        <Header
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          userName="PH_User"
+        />
+        <Main />
+      </section>
+      <About />
+      <Footer />
+    </>
   );
 }
 
